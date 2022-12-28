830fc72a20abf955040ccd77dde393563805a42e
Hozo45460 <hozo45460@gmail.com>
2022-12-28 17:16:12 +0700

    up8

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-$(window).on("load", function() {
-    $(".loader-container").fadeIn(1500).delay(1000).fadeOut("slow");
+$(window).load('load', function() {
+    $('.loader-container').fadeIn(1000).delay(1000).fadeOut('slow');
 })
